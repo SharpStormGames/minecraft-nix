8f653849087f49a89f8687461ba2db0eaa391c08
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2024-08-24 10:52:18 +1000

    Remove Checks, Tests & Flake Cleanup

--- test.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p parallel
-set -eu
-rm -rf "test"
-mkdir "test"
-cd "test"
-nix eval .\#checks.x86_64-linux --apply builtins.attrNames --json | jq -r .[] \
-    | parallel bash ../test-one.sh
