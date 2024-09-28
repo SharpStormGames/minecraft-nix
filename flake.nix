facb4964916caac56ebfdd10ddc08cc4a1628a2a
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2024-09-28 13:52:40 +1000

    Flake: Add homeManagerModules.default & devshell

--- flake.nix
+++ flake.nix
@@ -6,7 +6,16 @@
   };
   outputs = { self, nixpkgs, mcversions }: let pkgs = import nixpkgs { system = "x86_64-linux"; }; in {
     homeManagerModules.minecraft = import ./module/module.nix { inherit (self.lib.x86_64-linux) baseModules; }; isNixOSModule = false;
+    homeManagerModules.default = self.homeManagerModules.minecraft;
     homeManagerModule = self.homeManagerModules.minecraft;
     lib.x86_64-linux = import ./lib.nix { inherit self pkgs mcversions; };
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs.python3Packages; [  
+        colorama
+        pyjwt
+        requests
+        virtualenv
+      ];
+    };
   };
 }
